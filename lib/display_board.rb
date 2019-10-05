a3f5bd4738e798861155bcc2fb0fab79383e5faf
Andrea S Barr <andreabarr87@gmail.com>
2019-10-05 17:31:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board(tic_tac_toe)
   tic_tac_toe = 
- "   |   |   
-  -----------
-      |   |   
-  -----------
-  "
+ puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
   puts tic_tac_toe
 end
